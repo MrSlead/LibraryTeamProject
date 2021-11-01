732c6f6951c0b8de9e7eb0e46aacf1347fa0251a
Alex <mordyasov.60@mail.ru>
2021-11-01 19:22:25 +0700

    Changed the return type to ResponseEntity in ClientController

--- src/main/java/com/dream/team/library/controller/ClientController.java
+++ src/main/java/com/dream/team/library/controller/ClientController.java
@@ -4,6 +4,8 @@ import com.dream.team.library.entity.authorization.Client;
 import com.dream.team.library.entity.authorization.Role;
 import com.dream.team.library.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -19,38 +21,58 @@ public class ClientController {
     }
 
     @GetMapping("id/{idClient}")
-    public Optional<Client> getClientById(@PathVariable Long idClient) {
-        return clientService.findById(idClient);
+    public ResponseEntity<Optional<Client>> getClientById(@PathVariable Long idClient) {
+        if(idClient == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Optional<Client> client = clientService.findById(idClient);
+
+        if(client.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(client, HttpStatus.OK);
     }
 
     @GetMapping("login/{login}")
-    public Optional<Client> getClientByLogin(@PathVariable String login) {
-        return clientService.findByLogin(login);
+    public ResponseEntity<Optional<Client>> getClientByLogin(@PathVariable String login) {
+        if(login == null || login.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Optional<Client> client = clientService.findByLogin(login);
+
+        if(client.isEmpty()) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(client, HttpStatus.OK);
     }
 
     @GetMapping("all")
-    public List<Client> findAll() {
-        return clientService.findAll();
+    public ResponseEntity<List<Client>> findAll() {
+        List<Client> clientList = clientService.findAll();
+
+        return new ResponseEntity<>(clientList, HttpStatus.OK);
     }
 
     @GetMapping("premium")
-    public List<Client> getPremiumClients() {
-        return clientService.findAllByRole(Role.PREMIUM);
+    public ResponseEntity<List<Client>> getPremiumClients() {
+        List<Client> clientList = clientService.findAllByRole(Role.PREMIUM);
+
+        return new ResponseEntity<>(clientList, HttpStatus.OK);
     }
 
     @GetMapping("ordinary")
-    public List<Client> getOrdinaryClients() {
-        return clientService.findAllByRole(Role.ORDINARY);
+    public ResponseEntity<List<Client>> getOrdinaryClients() {
+        List<Client> clientList = clientService.findAllByRole(Role.ORDINARY);
+
+        return new ResponseEntity<>(clientList, HttpStatus.OK);
     }
 
     @PostMapping("add")
-    public Optional<Client> addClient(@RequestBody Client client) {
-        if(client == null) {
-            return Optional.of(new Client());
+    public ResponseEntity<Optional<Client>> addClient(@RequestBody Client client) {
+        Optional<Client> clientOptional = Optional.of(client);
+        if(clientOptional.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        client.setRole(Role.ORDINARY);
+        clientOptional.get().setRole(Role.ORDINARY);
+        clientService.save(clientOptional.get());
 
-        return clientService.save(client);
+        return new ResponseEntity<>(clientOptional, HttpStatus.OK);
     }
 }
