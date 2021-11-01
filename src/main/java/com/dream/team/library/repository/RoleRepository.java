31582e11b00a656bb8e8f89a7380a6f26d95375a
Alex <mordyasov.60@mail.ru>
2021-11-01 15:06:16 +0700

    Deleted the roleRepo (its enum, no repo) and added logging the sql requests

--- src/main/java/com/dream/team/library/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dream.team.library.repository;
-
-import com.dream.team.library.entity.authorization.Role;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends EntityRepository<Role> {
-}
