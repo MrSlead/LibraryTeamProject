c7f15ee5de0fb870724b78830e8495d0461afba8
evgeny_mordyasov <michisig11i7@gmail.com>
2021-12-11 21:08:39 +0700

    ORM fix

--- src/main/java/com/dream/team/library/entity/lib/Language.java
+++ src/main/java/com/dream/team/library/entity/lib/Language.java
@@ -1,15 +1,12 @@
 package com.dream.team.library.entity.lib;
 
 import com.dream.team.library.entity.AbstractEntity;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Getter
